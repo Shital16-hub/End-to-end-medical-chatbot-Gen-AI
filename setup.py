86f44b9c83e78ee9be773b19bdf6561857ae39b9
Shital N <shitalnandre108@gmail.com>
2025-02-01 20:02:27 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.1',
+    author = 'Shital',
+    packages = find_packages(),
+    install_requires = []
+)
